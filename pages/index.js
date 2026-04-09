a66c9b59ef9b30387153de78a09f0a7cd139f880
Praveenan Jeevarethinam <praveenanjvp@gmail.com>
2026-04-09 12:32:37 +0530

    Initial commit: Shipment Tracking project setup

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Shipment Tracking DApp
+      </h1>
+    </div>
+  )
+}
