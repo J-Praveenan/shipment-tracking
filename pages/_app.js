7acba6c924ed52ce85c2a5ddce91333aedc15b05
Praveenan Jeevarethinam <praveenanjvp@gmail.com>
2026-04-11 12:57:30 +0530

    feat: set up frontend components structure and include project assets

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import "../styles/globals.css";
 
+// INTERNAL IMPORT
+import {TrackingProvider} from "../Context/Tracking";
+import { NavBar } from "../Components/NavBar";
+import { Footer } from "../Components/Footer";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <TrackingProvider>
+        <NavBar/>
+        <Component {...pageProps} />
+      </TrackingProvider>
+      <Footer/>
+    </>
+  );
 }
